e67c81141382112ff96d4686671bcce2b3f68780
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2025-02-13 03:50:17 +0500

    delete comments

--- config/db.js
+++ config/db.js
@@ -3,11 +3,6 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// const sequelize = new Sequelize("my_db", "admin", "Ac_1267981", {
-//   host: "database-1.cfc4g44euxu4.eu-north-1.rds.amazonaws.com",
-//   dialect: "mysql",
-// });
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
